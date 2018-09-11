c3382128790b190d2d586276913804e8cb3e9860
Raúl Feliz <raulfeliz@gmail.com>
2018-09-11 21:54:11 +0100

    dagger configuration

--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/MainActivity.kt
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/MainActivity.kt
@@ -8,10 +8,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : DaggerAppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/di/modules/CocinaConRollModule.kt
+++ app/src/main/java/com/rukiasoft/androidapps/cocinaconroll/di/modules/CocinaConRollModule.kt
@@ -16,7 +16,6 @@ import javax.inject.Singleton
  */
 
 @Module(includes = [(ViewModelModule::class)])
-@Singleton
 class CocinaConRollModule {
 
 
